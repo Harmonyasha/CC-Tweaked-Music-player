6d6f7f51fc5a982aad0eccef57c544177572ebf9
OwlUniversal <71132236+OwlUniversal@users.noreply.github.com>
2024-03-30 02:47:39 +0300

    Update main.py
    Removed more useless things

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify, request, render_template,send_file
 app = Flask(__name__,static_url_path='')
-currentmsg = ""
 
 @app.route("/audio/<name>",methods=["post"])
 def audio(name):
   return send_file(name, as_attachment=True)
-  #return jsonify(lines)
   
 
 if __name__ == "__main__":
